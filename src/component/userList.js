1be55ec58c136498afb9af1d5e42f6904627a33a
mangeshipatil <thisismangeshi@gmail.com>
2022-11-24 22:45:03 +0530

    Added delete functionality

--- src/component/userList.js
+++ src/component/userList.js
@@ -1,64 +1,103 @@
 import { useDispatch, useSelector } from "react-redux"
 import "./style.css"
-import { set_celebrities } from "../redux/actions/celebrityAction"
+import { set_celebrities, remove_selected_celebrity } from "../redux/actions/celebrityAction"
 import celebrityData from "../json/celebrity.json"
 import Accordion from 'react-bootstrap/Accordion';
 import { GrEdit } from 'react-icons/gr';
-import { RiDeleteBin6Line } from 'react-icons/ri'
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useEffect, useState } from "react";
+
 
 const UserList = () => {
     const data = useSelector((state) => state.setCelebrity.celebrityData)
     const dispatch = useDispatch()
-    dispatch(set_celebrities(celebrityData))
 
-    console.log(data)
+    useEffect(()=>{
+        dispatch(set_celebrities(celebrityData))
+        // eslint-disable-next-line
+    },[])
 
+    const [show, setShow] = useState(false);
+    const [id, setId] = useState()
+
+
+    const handleClose = () => setShow(false);
+    const handleShow = (id) => {
+        setId(id)
+        setShow(true);
+    }
+    
+    const handleDelete = () => {
+        dispatch(remove_selected_celebrity(id))
+        setShow(false)
+    }
     return (
         <>
+
+            {show && (
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Body>Are you sure you want to delete?</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button variant="danger" onClick={() => handleDelete()}>
+                            Delete
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            )}
+
             <div className="containr d-flex justify-content-center align-items-center flex-column " >
                 <div className="accordion acc w-100 " id="accordionExample">
-                    {data.map((item) => {
+                    {data.map((item, id) => {
                         return (
-                            <Accordion className="accordion acc1 ">
-                                <Accordion.Item eventKey="0">
-                                    <Accordion.Header>
-                                        <img className="image img-fluid" src={item.picture} alt="" />
-                                        <div className="full-name"> {`${item.first} ${item.last} `}</div>
-                                    </Accordion.Header>
-                                    <Accordion.Body>
-                                        <div className="main d-flex flex-column">
-                                            <div className="top-section d-flex">
-                                                <div className="age-cont col-md-4 col-4">
-                                                    <div className="col">Age</div>
-                                                    <div className="age">22</div >
+                            <div key={id}>
+                                <Accordion className="accordion acc1 ">
+                                    <Accordion.Item eventKey="0">
+                                        <Accordion.Header>
+                                            <img className="image img-fluid" src={item.picture} alt="" />
+                                            <div className="full-name"> {`${item.first} ${item.last} `}</div>
+                                        </Accordion.Header>
+                                        <Accordion.Body>
+                                            <div className="main d-flex flex-column">
+                                                <div className="top-section d-flex">
+                                                    <div className="age-cont col-md-4 col-4">
+                                                        <div className="col">Age</div>
+                                                        <div className="age">22</div >
+                                                    </div>
+                                                    <div className="gender-cont col-md-4 col-4">
+                                                        <div className="col">Gender</div>
+                                                        <div className="gender">{item.gender}</div >
+                                                    </div>
+                                                    <div className="country-cont col-md-4 col-4">
+                                                        <div className="col">Country</div>
+                                                        <div className="country">{item.country}</div>
+                                                    </div>
                                                 </div>
-                                                <div className="gender-cont col-md-4 col-4">
-                                                    <div className="col">Gender</div>
-                                                    <div className="gender">{item.gender}</div >
+                                                <div className="bottom-section">
+                                                    <div className="col">Description</div>
+                                                    <div>{item.description}</div>
                                                 </div>
-                                                <div className="country-cont col-md-4 col-4">
-                                                    <div className="col">Country</div>
-                                                    <div className="country">{item.country}</div>
+                                                <div className="icon-cont d-flex justify-content-end">
+                                                    <div className="edit-icon">
+                                                        <GrEdit />
+                                                    </div>
+                                                    <div className="delete-icon">
+                                                        <RiDeleteBin6Line onClick={()=>handleShow(id)} />
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div className="bottom-section">
-                                                <div className="col">Description</div>
-                                                <div>{item.description}</div>
-                                            </div>
-                                            <div className="icon-cont d-flex justify-content-end">
-                                                <div className="edit-icon">
-                                                    <GrEdit />
-                                                </div>
-                                                <div className="delete-icon">
-                                                    <RiDeleteBin6Line />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                            </Accordion>
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                </Accordion>
+                            </div>
                         )
+
                     })}
+
                 </div>
 
             </div>
--- src/redux/reducer/CelebrityReducer.js
+++ src/redux/reducer/CelebrityReducer.js
@@ -1,11 +1,23 @@
 const initialState = {
-    celebrityData:[]
+    celebrityData: []
 }
 
-export const setCelebrity=(state=initialState,action)=>{
-    switch(action.type){
+
+export const setCelebrity = (state = initialState, action) => {
+    switch (action.type) {
         case "SET_CELEBRITY":
-            return {...state,celebrityData:action.payload}
+            return {
+                ...state, celebrityData: action.payload
+            }
+
+        case "REMOVE_SELECTED_CELEBRITY":
+            const newstate=state.celebrityData.filter((item, index) => index !== action.payload)
+            console.log(newstate)
+        return {
+            ...state,
+            celebrityData: newstate
+            
+        }
         default:
             return state
     }
